5bd88f4427a682eaf82cc75062af5dc888636ad3
Вадим Коппе <vadimkoppe@macbook.local>
2016-09-23 22:28:01 +0300

    Removed vars of constraints.

--- Youtube/ViewController.m
+++ Youtube/ViewController.m
@@ -27,50 +27,41 @@
     
     [self.youtube setupVideoId:@"GBa2sS2EKAg"];
     
-    NSLayoutConstraint *topConstraint;
-    NSLayoutConstraint *leftConstraint;
-    NSLayoutConstraint *rightConstraint;
-    NSLayoutConstraint *bottomConstraint;
-    
     // Setting up left constraint
-    leftConstraint = [NSLayoutConstraint constraintWithItem:self.youtube
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.youtube
                                                   attribute:NSLayoutAttributeLeft
                                                   relatedBy:NSLayoutRelationEqual
                                                      toItem:self.view
                                                   attribute:NSLayoutAttributeLeft
                                                  multiplier:1.0
-                                                   constant:0];
-    [self.view addConstraint:leftConstraint];
+                                                           constant:0]];
     
     // Setting up right constraint
-    rightConstraint = [NSLayoutConstraint constraintWithItem:self.youtube
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.youtube
                                                    attribute:NSLayoutAttributeRight
                                                    relatedBy:NSLayoutRelationEqual
                                                       toItem:self.view
                                                    attribute:NSLayoutAttributeRight
                                                   multiplier:1.0
-                                                    constant:0];
-    [self.view addConstraint:rightConstraint];
+                                                           constant:0]];
     
     // Setting up top constraint
-    topConstraint = [NSLayoutConstraint constraintWithItem:self.youtube
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.youtube
                                                  attribute:NSLayoutAttributeTop
                                                  relatedBy:NSLayoutRelationEqual
                                                     toItem:self.view
                                                  attribute:NSLayoutAttributeTop
                                                 multiplier:1.0
-                                                  constant:0];
-    [self.view addConstraint:topConstraint];
+                                                           constant:0]];
     
     // Setting up bottom constraint
-    bottomConstraint = [NSLayoutConstraint constraintWithItem:self.youtube
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.youtube
                                                     attribute:NSLayoutAttributeBottom
                                                     relatedBy:NSLayoutRelationEqual
                                                        toItem:self.view
                                                     attribute:NSLayoutAttributeBottom
                                                    multiplier:1.0
-                                                     constant:0];
-    [self.view addConstraint:bottomConstraint];
+                                                           constant:0]];
     
     self.youtube.translatesAutoresizingMaskIntoConstraints = NO;
 }
